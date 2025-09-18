03ee7b863b802093068e1434ed05bcce7b9eaaad
Om Tiwari <omtiwari9755@gmail.com>
2025-09-19 04:00:14 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -111,7 +111,7 @@ const App = () => {
         </select>
       </div>
 
-      {selectedCity ? (
+      {selectedCity && selectedState && selectedCountry ? (
         <h1>
             You selected {selectedCity}, {selectedState}, {selectedCountry}
         </h1>
